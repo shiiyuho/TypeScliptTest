854c6ab522b90d30e84102c8d83be064f26626d8
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-08 22:17:40 +0900

    環境設定　no-de moduiesを「ignore」設定

--- 算術演算関数モジュールの作成/calculations.js
+++ 算術演算関数モジュールの作成/calculations.js
@@ -1,14 +1,12 @@
+"use strict";
 //calculations.tsファイルから関数をエクスポートする文
-
 let x = 0;
 let y = 0;
 //加算関数（add）
-const getRectang =function(x:number,y:number):number{
-    return x+y;
-}
-console.log(getRectang(3,5));
+const getRectang = function (x, y) {
+    return x + y;
+};
+console.log(getRectang(3, 5));
 //減算関数（subtract）
-
 //乗算関数（multiply）
-
 //除算関数（divide）
