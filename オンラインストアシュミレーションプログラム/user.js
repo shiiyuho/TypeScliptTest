192c00ce8ea84d4600e10b79547f3d38a36acb01
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-09 17:01:02 +0900

    コミット

--- オンラインストアシュミレーションプログラム/user.js
+++ オンラインストアシュミレーションプログラム/user.js
@@ -1,1 +1,11 @@
 "use strict";
+class User {
+    constructor() {
+        this.ID = 0;
+        this.name = "";
+        this.mail = "";
+    }
+    displayInfo() {
+        console.log(`title: ${this.ID}, author: ${this.name},this:${this.mail}`);
+    }
+}
