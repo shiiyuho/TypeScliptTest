89db5a3de082d2b430bac70aa53383baa6690863
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-10 08:39:17 +0900

    コメントを追加
    「main.ts」と「user..ts」にコメントを追加

--- オンラインストアシュミレーションプログラム/main.ts
+++ オンラインストアシュミレーションプログラム/main.ts
@@ -1,8 +1,8 @@
-import { product } from "./product.js";
-import { user } from "./user.js";
-import { processPurchase } from "./purchase.js";
+import { product } from "./product.js"; //product.tsのインポート文
+import { user } from "./user.js"; //user.tsのインポート文
+import { processPurchase } from "./purchase.js"; //Purchaseのインポート文
 
-const sampleProduct = new product(1, "Nakano Nino", 28000, 5);
-const sampleUser = new user(101, "Ishii", "ishiiufo.2@icloud.com");
+const sampleProduct = new product(1, "Nakano Nino", 28000, 5); //productへの値代入（ID、名前、値段、個数）
+const sampleUser = new user(101, "Ishii", "ishiiufo.2@icloud.com"); //userへの値入力（ID、名前、メルアド）
 
-processPurchase(sampleUser.ID, sampleProduct);
+processPurchase(sampleUser.ID, sampleProduct); //processPurchaseの関数呼び出し
