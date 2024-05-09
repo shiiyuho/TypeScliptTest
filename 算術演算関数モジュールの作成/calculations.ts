47e290fc367f2da6bbc1654d2a5b280fad849bc2
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-09 10:16:30 +0900

    インデント設定

--- 算術演算関数モジュールの作成/calculations.ts
+++ 算術演算関数モジュールの作成/calculations.ts
@@ -5,12 +5,11 @@ export function calculation(number1: number, number2: number) {
         number1 * number2,  //掛け算
         number1 / number2,  //割り算
     ]
-    if(number2 != 0){
+    if (number2 != 0){
         return answer;  //答えを戻す戻り値
     }else{
         console.log('error');
     }
-    
   }
   
-  
+
--- 算術演算関数モジュールの作成/test.js
+++ 算術演算関数モジュールの作成/test.js
@@ -1,4 +1,4 @@
 //test.tsファイルで、インポートした関数を呼び出してテストするコード
 import { calculation } from "./calculations.js";
-const answer = calculation(10, 0); //値の代入
+const answer = calculation(10, 10); //値の代入
 console.log(answer); //答えの出力
