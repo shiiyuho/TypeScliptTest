a0d77c6047b2127971c8c558d7e7444d838d1ab4
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-10 10:05:54 +0900

    最終コミット

--- 算術演算関数モジュールの作成/test.js
+++ 算術演算関数モジュールの作成/test.js
@@ -1,15 +1,20 @@
 //test.tsファイルで、インポートした関数を呼び出してテストするコード
 import { add } from "./calculations.js";
+import { subtract } from "./calculations.js";
+import { multiply } from "./calculations.js";
+import { divide } from "./calculations.js";
+//add(足し算)
 const a = add(10, 10); //値の代入
 console.log(a); //答えの出力
-import { subtract } from "./calculations.js";
+//subtract(引き算)
 const b = subtract(10, 0); //値の代入
 console.log(b);
-import { multiply } from "./calculations.js";
+//multiply(掛け算)
 const c = multiply(10, 10); //値の代入
 console.log(c);
-import { divide } from "./calculations.js";
+//divide(割り算)
 const d = divide(10, 10); //値の代入
+//第2引数が0だった場合
 const e = divide(10, 0);
 console.log(d);
 console.log(e);
