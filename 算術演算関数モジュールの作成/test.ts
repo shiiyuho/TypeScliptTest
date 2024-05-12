951a723a8a8e34006f67bac63f32d86874f1df95
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-12 21:37:38 +0900

    「計算をさせる値の出力」を追加

--- 算術演算関数モジュールの作成/test.ts
+++ 算術演算関数モジュールの作成/test.ts
@@ -6,19 +6,23 @@ import { divide } from "./calculations.js";
 
 //add(足し算)
 const a = add(10, 10); //値の代入
+console.log("計算をさせる値:10,10");
 console.log("add関数の出力結果:", a); //答えの出力
 
 //subtract(引き算)
 const b = subtract(10, 0); //値の代入
+console.log("計算をさせる値:10,0");
 console.log("subtract関数の出力結果:", b);
 
 //multiply(掛け算)
 const c = multiply(10, 10); //値の代入
+console.log("計算をさせる値:10,10");
 console.log("multiply関数の出力結果:", c);
 
 //divide(割り算)
 const d = divide(10, 10); //値の代入
+console.log("計算をさせる値:10,10");
 console.log("divide関数の出力結果:", d);
 //第2引数が0だった場合
+console.log("計算をさせる値:10,0");
 const e = divide(10, 0);
-// console.log("divide関数（引数2が「0」の場合の出力結果:", "error");
